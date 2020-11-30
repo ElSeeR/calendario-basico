1201b83e2de7098db96ad953d12678f7338537ae
ElSeeR <sergio9garcia9@gmail.com>
2020-11-30 10:07:30 +0100

    fecha

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -8,4 +8,29 @@ public class CalendarioBasico{
         mes = 1;
         ano = 1;
     }
+    
+    public String obtenerFecha(){
+        return dia + "-" + mes + "-" + ano;
+    }
+    
+    public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
+    }
+    
+    public void avanzarFecha(){
+    dia = dia + 1;
+    if(dia > 30){
+        mes = mes + 1;
+        dia = 1;
+        if(mes > 12){
+            mes = 1;
+            ano = ano + 1;
+            if(ano > 99){
+                ano = 1;
+            }
+        }
+    }
+    }
 }
