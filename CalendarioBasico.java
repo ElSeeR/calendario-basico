33eeb1f9771db315a8ad107456111edc95f29462
ElSeeR <sergio9garcia9@gmail.com>
2020-11-30 09:18:15 +0100

    0321.02 atributos

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,4 +1,11 @@
 public class CalendarioBasico{
+    private int dia;
+    private int mes;
+    private int ano;
 
-
+    public CalendarioBasico(){
+        dia = 1;
+        mes = 1;
+        ano = 1;
+    }
 }
